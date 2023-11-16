82e00cc0a6d2e277e1c3c150ee3058e3f781859a
sasegura <sasegura.fernandez87@gmail.com>
2023-11-16 13:34:07 +0100

    links

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 
-import theme from "./assets/theme";
-import Presentation from "./layouts/pages/presentation";
-import routes from "./routes";
+import theme from './assets/theme'
+import Presentation from './layouts/pages/presentation'
+import routes from './routes'
 
-import "./App.css";
-import Layout from "./layouts/pages/layout";
-import { pagesRoutes } from "./utils/const/pageRoutes";
+import './App.css'
+import Layout from './layouts/pages/layout'
+import { pagesRoutes } from './utils/const/pageRoutes'
+import HomePage from './pages/Home'
 
 export default function App() {
   const getRoutes = (allRoutes: any[]): any =>
     allRoutes.map((route: any) => {
       if (route.collapse) {
-        return getRoutes(route.collapse);
+        return getRoutes(route.collapse)
       }
 
       if (route.route) {
-        return (
-          <Route path={route.route} element={route.component} key={route.key} />
-        );
+        return <Route path={route.route} element={route.component} key={route.key} />
       }
 
-      return null;
-    });
+      return null
+    })
 
   return (
     <ThemeProvider theme={theme}>
@@ -35,10 +34,11 @@ export default function App() {
             <Route key={path} path={path} element={element} />
           ))}
 
+          <Route path="/portfolio" element={<HomePage />} />
           <Route path="/presentation" element={<Presentation />} />
-          <Route path="*" element={<Navigate to="/presentation" />} />
+          <Route path="*" element={<Navigate to="/portfolio" />} />
         </Routes>
       </Layout>
     </ThemeProvider>
-  );
+  )
 }
